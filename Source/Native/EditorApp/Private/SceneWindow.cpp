6531ab0922e2a16591c198803b0ee4941df0c310
stalomeow <stalowork@163.com>
2024-10-12 14:34:04 +0800

    chore: add summary

--- Source/Native/EditorApp/Private/SceneWindow.cpp
+++ Source/Native/EditorApp/Private/SceneWindow.cpp
@@ -19,6 +19,12 @@ using namespace DirectX;
 
 namespace march
 {
+    // Scene Window 几个要点：
+    // 1. 开多个 Scene Window 时，各种操作不能相互影响
+    // 2. 必须在 Scene View Image 上滚动鼠标滚轮才能移动相机
+    // 3. 必须从 Scene View Image 上**开始**拖拽才能转动相机，之后鼠标可以拖出窗口
+    // 4. 如果窗口正在移动，不允许操作相机
+
     SceneWindow::SceneWindow()
         : m_EnableMSAA(true)
         , m_Display(nullptr)
